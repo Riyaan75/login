4bd1f551b9dd42c962c1ac3bcc032d00f6363466
Riyaan75 <riyaan75.RH@gmail.com>
2022-11-21 21:56:28 +0530

    views

--- index.js
+++ index.js
@@ -3,17 +3,35 @@ const express = require("express");
 const MDB = require("./database/client");
 const port = process.env["PORT"] || 3000;
 const app = express();
-app.use(express.urlencoded({ extended: true }))
+const ejs = require("ejs");
+const bodyParser = require("body-parser");
+
+
+//app.engine("ejs", ejsMate);
+app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.json())
 const userRouter = require("./Routes/login");
 
 app.use("/users", userRouter);
 
-app.get('/',(req,res)=>{
-  res.status(200).send({
-    "Message":"Server Up and Running for DITU-Help-Crunch"
-  })
+app.get("/",function(req,res){
+  res.render("index");
 })
+
+app.get("/login", function(req,res){
+  res.render("login");
+})
+
+app.get("/register", function(req,res){
+  res.render("register");
+})
+
+// app.get('/',(req,res)=>{
+//   res.status(200).send({
+//     "Message":"Server Up and Running for DITU-Help-Crunch"
+//   })
+// })
 app.listen(port, async () => {
   await MDB.connect();
   console.log(`Server litening to port:${port}`);
